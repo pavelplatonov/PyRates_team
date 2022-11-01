c77e04435fb090b8e06f8ad0f29f969e08d1e02c
zastasja <anastasia_zh@bk.ru>
2022-11-01 23:28:56 +0300

    added of test TC_001.00.05

--- SwagLabs_Project /test_login.py
+++ SwagLabs_Project /test_login.py
@@ -1,5 +1,6 @@
 from pages.locators import LoginLocators
 from pages.login_page import LoginPage
+from time import sleep
 
 link = 'https://www.saucedemo.com/'
 valid_user = 'standard_user'
@@ -19,6 +20,7 @@ def test_login_valid_user(browser):
     page.sendkeys_element(*LoginLocators.login_field, valid_user)
     page.sendkeys_element(*LoginLocators.password_field, password)
     page.click_element(*LoginLocators.login_btn)
+    page.should_be_current_page('https://www.saucedemo.com/inventory.html')
 
 def test_locked_out_user(browser):
     page = LoginPage(browser, link)
@@ -33,6 +35,7 @@ def test_problem_user(browser):
     page.sendkeys_element(*LoginLocators.login_field, problem_user)
     page.sendkeys_element(*LoginLocators.password_field, password)
     page.click_element(*LoginLocators.login_btn)
+    page.should_be_current_page('https://www.saucedemo.com/inventory.html')
 
 def test_performance_glitch_user(browser):
     page = LoginPage(browser, link)
@@ -40,3 +43,13 @@ def test_performance_glitch_user(browser):
     page.sendkeys_element(*LoginLocators.login_field, performance_glitch_user)
     page.sendkeys_element(*LoginLocators.password_field, password)
     page.click_element(*LoginLocators.login_btn)
+    page.should_be_current_page('https://www.saucedemo.com/inventory.html')
+
+def test_login_valid_user_empty_password(browser):
+    page = LoginPage(browser, link)
+    page.open_login_page()
+    page.sendkeys_element(*LoginLocators.login_field, valid_user)
+    page.click_element(*LoginLocators.login_btn)
+    error_text = page.find_element_text(*LoginLocators.error_warning)
+    assert error_text == 'Epic sadface: Password is required', "wrong warning text"
+    sleep(5)
