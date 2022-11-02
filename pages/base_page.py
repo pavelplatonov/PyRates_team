4eebc41596c144d1f1608d493e6f8b94911d61eb
zastasja <anastasia_zh@bk.ru>
2022-11-02 16:46:42 +0300

    added annotation to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,14 +8,29 @@ class BasePage():
         self.browser = browser
         self.link = link
 
+
+    """Открывает ссылку"""
+
     def open_page(self):
         self.browser.get(self.link)
+
+
+    """Нажатие на выбранный элемент из локаторов"""
+
 
     def click_element(self, method, locator):
         self.browser.find_element(method, locator).click()
 
+
+    """Передает текст или нажание кнопок на клавиатуре"""
+
+
     def sendkeys_element(self, method, locator, text):
         self.browser.find_element(method, locator).send_keys(text)
+
+
+    """Подтверждение наличия элемента на странице"""
+
 
     def element_is_present(self, method, locator):
         try:
@@ -23,6 +38,10 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+
+    """Подтверждение текущего адреса страницы"""
+
 
     def should_be_current_page(self, link):
         assert link in self.browser.current_url, 'wrong url'
